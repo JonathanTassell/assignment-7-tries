31379f581e7aa6414df5510e1dc9b3c6d080d841
JonathanTassell <115473629+JonathanTassell@users.noreply.github.com>
2023-10-09 16:05:30 +1030

    Update Autocomplete.h

--- Autocomplete.h
+++ Autocomplete.h
@@ -1,45 +1,63 @@
 #ifndef AUTOCOMPLETE_H
-#define AUTOCOMPLETE_H
+#define AUTOCOMPLETE_H 
 
-#include <vector>
-#include <iostream>
-#include <string>
+#include <vector> 
+#include <string> 
 
 const int alphabetSize = 26; 
 
-class Autocomplete{
+class Autocomplete{ 
     private:
         struct Node{
+            //address of nodes child/children
             std::vector<Node*> children; 
-            bool endOfWord; 
+            //if end of word then true 
+            bool isEndOfWord; 
         }; 
 
-        Node* root; 
+        //root of Autocomplete
+        
+       
     
-    public:
-        Autocomplete(); 
+    public: 
+        Node* root; 
+        //constructor setting root's children to nullptr
+        Autocomplete();      
+    
 
+
+        //insert function 
         void insert(std::string word); 
 
-        void printNode(int num); 
-
+        //helper function for get suggestions
         std::vector<std::string> suggestionRecursive(Node* currNode, const std::string currPrefix);
 
+        //get Suggestions main function 
         std::vector<std::string> getSuggestions(std::string partialWord); 
 
-        //initialises
+
+
+
+
+        /***** STRUCTS AND TESTING *****/
+
+
+        //creates new node intialised with nullptrs
         struct Node* createNode(){
             struct Node* newNode = new Node; 
 
             for(int i=0; i<alphabetSize; i++){
                 newNode->children.push_back(nullptr); 
             }
-            newNode->endOfWord = false; 
+            newNode->isEndOfWord = false; 
             
             return newNode; 
         }
-
-
+        
+        //for testing
+        Node* returnRoot(){
+            return root; 
+        }
 }; 
 
 #endif
